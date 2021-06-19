45e6b96fdee537761ece4948d292684471e4185b
Alex Rybachev <rybachev.as@gmail.com>
2021-06-19 11:41:52 +0400

    Initial Commit

---
+++ GeometryAnimationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryAnimationApp
+//
+//  Created by Aleksandr Rybachev on 19.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeometryAnimationApp/GeometryAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryAnimationAppApp.swift
+//  GeometryAnimationApp
+//
+//  Created by Aleksandr Rybachev on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct GeometryAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
